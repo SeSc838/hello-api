f043e0054d50691d0ac3b9c615a9b76d551075a9
Sebastian Schweiger <sesc838@gmail.com>
2025-12-28 10:17:52 +0100

    3.6 Adding it to the pipeline

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"hello-api/handlers/rest"
 	"log"
 	"net/http"
 )
@@ -11,24 +10,8 @@ func main() {
 	addr := ":8080"
 
 	mux := http.NewServeMux()
-
-	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		enc := json.NewEncoder(w)
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		resp := Resp{
-			Language:    "English",
-			Translation: "Hello",
-		}
-		if err := enc.Encode(resp); err != nil {
-			panic(fmt.Sprintln("unable to encode response", err))
-		}
-	})
+	mux.HandleFunc("/hello", rest.TranslateHandler)
 
 	log.Printf("listening on %s\n", addr)
 	log.Fatal(http.ListenAndServe(addr, mux))
-}
-
-type Resp struct {
-	Language    string `json:"language"`
-	Translation string `json:"translation"`
 }
