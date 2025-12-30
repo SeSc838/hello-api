8973b321beefc5a6dffe8acd1a0bed8525eb0715
Sebastian Schweiger <sesc838@gmail.com>
2025-12-30 23:53:02 +0000

    Just for testing

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
   };
+  
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
